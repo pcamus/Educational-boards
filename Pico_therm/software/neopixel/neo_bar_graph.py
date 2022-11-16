0a9f79d634c51c065e663fa1f921827b720415a2
Philippe CAMUS <philippe.camus@hepl.be>
2022-11-16 08:12:13 +0100

    Corrected to remove global variables

--- Pico_therm/software/neopixel/neo_bar_graph.py
+++ Pico_therm/software/neopixel/neo_bar_graph.py
@@ -8,6 +8,7 @@
 #http://www.pibits.net/code/raspberry-pi-pico-and-neopixel-example-in-micropython.php#codesyntax_1
 import array, time
 from machine import Pin
+import rp2
  
 # Configure the number of WS2812 LEDs, pins and brightness.
 NUM_LEDS = 8
@@ -15,6 +16,8 @@ PIN_NUM = 1
 brightness = 0.05 # from 0 to 1
 
 #Neopixel protocol is controlled by a pio machine
+# Genral Info about rp2: https://docs.micropython.org/en/latest/library/rp2.html#module-rp2
+# Specific info for RP2040 : https://docs.micropython.org/en/latest/library/rp2.StateMachine.html
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
 def ws2812():
     T1 = 2
@@ -36,19 +39,6 @@ sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(PIN_NUM))
 # Start the StateMachine, it will wait for data on its FIFO.
 sm.active(1)
  
-# Display a pattern on the LEDs via an array of LED RGB values.
-# Array type, see : https://docs.python.org/3/library/array.html
-ar = array.array("I",[0]*NUM_LEDS) # type unsigned integer, initialized with a list of 0
-
-
-# 24 bit data for each neopixel in the GRB order
-def pixel_set_and_dim(color, brightness):
-    green=int(color[1]*brightness)
-    red=int(color[0]*brightness)
-    blue=int(color[2]*brightness)
-    result= (green<<16)+(red<<8)+blue
-    return result
-
 # For the bar graph display with 8 pixels
 # Color values (add color if necessary)
 BLACK = (0, 0, 0)
@@ -74,18 +64,29 @@ BG7 = (BLUE1, BLUE2, YELLOW1 ,YELLOW2 ,ORANGE1 ,ORANGE2 ,RED1 ,RED2)
 
 BG_LIST= [BG0, BG1, BG2, BG3, BG4, BG5, BG6, BG7] # and also modify the list
 
-# used to test library
-def draw_bar_graph(level):
-    BG_DISP= BG_LIST[level]
-    print(level)
-    for i, color in enumerate(BG_DISP):
+# 24 bit data for each neopixel in the GRB order
+def pixel_set_and_dim(color, brightness):
+    green=int(color[1]*brightness)
+    red=int(color[0]*brightness)
+    blue=int(color[2]*brightness)
+    result= (green<<16)+(red<<8)+blue
+    return result
+
+# Used to test library : draws successively all the possible bars
+def draw_bar_graph(bar):
+    # Array type, see : https://docs.python.org/3/library/array.html
+    ar = array.array("I",[0]*NUM_LEDS) # type unsigned integer, initialized with a list of 0
+
+    for i, color in enumerate(bar):
         ar[i]=pixel_set_and_dim(color,brightness)
         
-    sm.put(ar, 8)
+    sm.put(ar, 8) # pushes a word of data to the state machine
+    # second parameter indicates a shift value for each pushed data (from ar).
+    # data is coded with 32 bits, so the value pushes the 24 bits at the right position.
 
 if __name__ == "__main__":
     # execute only if run as the main module (i.e. not an import module)
     for i in range(8):
-        draw_bar_graph(i)
+        draw_bar_graph(BG_LIST[i])
         time.sleep(1)
-        
+       
